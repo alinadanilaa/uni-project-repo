fd80c2297f45a042c67f4608c2bb7d8cfc531e44
alinadanilaa <75304844+alinadanilaa@users.noreply.github.com>
2023-10-03 10:43:15 +0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+Numele este: <?php echo $_GET["nume"]; ?><br />
+Prenumele este: <?php echo $_GET["prenume"]; ?><br />
+Varsta este: <?php echo $_GET["varsta"]; ?><br />
+</body>
+</html>
